b74012eaf6b7238a765190b10a0a732cb679477d
Trishal hegde <trishalhegde@gmail.com>
2025-09-22 21:33:47 +0530

    import two

--- Dataclean.py
+++ Dataclean.py
@@ -3,4 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df=pd.read_csv('Titanic-Dataset.csv')
+print("Dataset Info")
 df.info()
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
